c1a08ce85f3432a9fc45c3ee357dcb6cb7079b73
Dwaraka224 <reddydwarakanath800@gmail.com>
2023-11-20 19:44:21 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def dwaraka(request):
+    return render(request,'dwaraka.html')
+
+def pavan(request):
+    return render(request,'pavan.html')
+def dwaraka1(request):
+    return render(request,'dwaraka1.html')
